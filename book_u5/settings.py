47ebec083c756d857e17bd46c97efe24b41fd6d4
Temur_dev <129756426+temuralidavron@users.noreply.github.com>
2025-07-28 15:31:54 +0500

    Update settings.py

--- book_u5/settings.py
+++ book_u5/settings.py
@@ -66,23 +66,23 @@ WSGI_APPLICATION = 'book_u5.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "nom",
-        "USER": "postgres",
-        "PASSWORD": "1995",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "nom",
+#         "USER": "postgres",
+#         "PASSWORD": "1995",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
